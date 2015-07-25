8bbe314a6a3a5ec672aad1fa89ea4ec0b0659539
Vincent Kee <vkee@mit.edu>
2015-07-25 19:49:26 -0400

    default with all options

--- keystone.js
+++ keystone.js
@@ -29,11 +29,13 @@ keystone.init({
 		extname: '.hbs'
 	}).engine,
 	
+	'emails': 'templates/emails',
+	
 	'auto update': true,
 	'session': true,
 	'auth': true,
 	'user model': 'User',
-	'cookie secret': '_hH"SnMh,S4vyezeklB^{+rC_#?pN]s5qD_FdT|(p.E|e[i("{"`Ms#"sxD?bI^.'
+	'cookie secret': 'A/OP*e)!R0^MKG&`H8MaSv=p_Dc|<(c=&EO&I7|nBhW}U9[kb^u1eoHE*mtR.(y('
 
 });
 
@@ -59,9 +61,44 @@ keystone.set('routes', require('./routes'));
 // Setup common locals for your emails. The following are required by Keystone's
 // default email templates, you may remove them if you're using your own.
 
+keystone.set('email locals', {
+	logo_src: '/images/logo-email.gif',
+	logo_width: 194,
+	logo_height: 76,
+	theme: {
+		email_bg: '#f9f9f9',
+		link_color: '#2697de',
+		buttons: {
+			color: '#fff',
+			background_color: '#2697de',
+			border_color: '#1a7cb7'
+		}
+	}
+});
+
+// Setup replacement rules for emails, to automate the handling of differences
+// between development a production.
+
+// Be sure to update this rule to include your site's actual domain, and add
+// other rules your email templates require.
+
+keystone.set('email rules', [{
+	find: '/images/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
+}, {
+	find: '/keystone/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
+}]);
+
+// Load your project's email test routes
+
+keystone.set('email tests', require('./routes/emails'));
+
 // Configure the navigation bar in Keystone's Admin UI
 
 keystone.set('nav', {
+	'posts': ['posts', 'post-categories'],
+	'galleries': 'galleries',
 	'enquiries': 'enquiries',
 	'users': 'users'
 });
